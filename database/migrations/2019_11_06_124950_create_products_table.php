c134ea8cf4503423713553969eac5a9d478a6409
sergiu5p <drehlutapetruta@gmail.com>
2019-11-13 11:39:31 +0200

    project done

--- database/migrations/2019_11_06_124950_create_products_table.php
+++ database/migrations/2019_11_06_124950_create_products_table.php
@@ -18,7 +18,7 @@ class CreateProductsTable extends Migration
             $table->string('title');
             $table->text('description');
             $table->unsignedBigInteger('price');
-            $table->string('image_url')->nullable();
+            $table->string('image_path')->nullable();
             $table->timestamps();
         });
     }
